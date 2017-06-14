a60fe4f81c69a3bcb7e3a9a6add36b68df5ba879
jamestford <james.t.ford@gmail.com>
2017-06-13 23:17:50 -0400

    added gnome-settings preference file

---
+++ gnome-settings.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------------
+# Created by James Ford mailto: jamestford[at]gmail[dot]com
+#-------------------------------------------------------------------------------
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# Change time format to 12 hours rather than 24
+sudo gsettings set org.gnome.desktop.interface clock-format 12h
